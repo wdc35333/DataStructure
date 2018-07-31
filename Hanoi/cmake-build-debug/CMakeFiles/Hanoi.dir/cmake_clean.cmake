6ab7a0bd07dc1df378d092140f56e37d9eade5f4
OafDChan <wdc35333@gmail.com>
2018-07-31 23:05:56 +0900

    하노이의 탑

---
+++ Hanoi/cmake-build-debug/CMakeFiles/Hanoi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hanoi.dir/main.c.o"
+  "Hanoi.pdb"
+  "Hanoi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hanoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
